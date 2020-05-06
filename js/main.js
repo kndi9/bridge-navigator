9734b8729be2273308d0155eec6c6fdf0d0c3c16
afzaman <afzaman2003@gmail.com>
2020-05-06 13:02:02 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+
+//Search structures.json and filter it
+const searchStructures = async searchText => {
+    const res = await fetch('../data/state-structures.json');
+    const structures = await res.json();
+    
+    //Get matches to current text input
+    let matches = structures.filter(bridge => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return bridge.structure.match(regex) || bridge.name.match(regex);
+    });
+    if(searchText.length < 4){
+        matches =[];
+        matchList.innerHTML = '';
+    }
+    outputHtml(matches);
+};
+
+//Show results in HTML
+const outputHtml = matches => {
+    if(matches.length > 0){
+        const html = matches.map(match => `
+        <div class="card card-body mb-1">
+            <h4>${match.name}<span class="text-primary"> ${match.structure}</span></h4>
+            <small>Milepost ${match.milepost}</small>
+            <small><a href="https://www.google.com/maps/search/?api=1&query=${match.latlong}">Lat/Long: ${match.latlong}</a></small>
+        </div>
+        `).join('');
+        matchList.innerHTML = html;
+    }
+}
+search.addEventListener('input', () => searchStructures(search.value))
+;
