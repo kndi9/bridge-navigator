32b533bc4ea6761bd233cf1dea660dedaaf9f4fc
afzaman <afzaman2003@gmail.com>
2020-05-18 14:21:57 -0400

    Expanded search function and added display fields

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # bridge-navigator
-A simple auto-complete app to navigate NJ Bridges for inspection purposes.
+A simple auto-complete app to navigate NJ Bridges and fueling stations for inspection purposes.
 
 Raw data can be found here: https://www.state.nj.us/transportation/refdata/sldiag/
+
 I learned the basiscs from here:https://www.theodinproject.com
+
 I used this as a template: https://www.youtube.com/watch?v=1iysNUrI3lw&list=WL&index=9&t=328s
--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const searchStructures = async searchText => {
     
     //Get matches to current text input
     let matches = structures.filter(bridge => {
-        const regex = new RegExp(`^${searchText}`, 'gi');
+        const regex = new RegExp(`${searchText}`, 'gi');
         return bridge.structure.match(regex) || bridge.name.match(regex);
     });
     if(searchText.length < 4){
@@ -24,12 +24,12 @@ const outputHtml = matches => {
         const html = matches.map(match => `
         <div class="card card-body mb-1">
             <h4>${match.name}<span class="text-primary"> ${match.structure}</span></h4>
-            <small>Milepost ${match.milepost}</small>
+            <small>${match.town}, ${match.county}</small>
+            <small>${match.route}, Milepost ${match.milepost}</small>
             <small><a href="https://www.google.com/maps/search/?api=1&query=${match.latlong}">Lat/Long: ${match.latlong}</a></small>
         </div>
         `).join('');
         matchList.innerHTML = html;
     }
 }
-search.addEventListener('input', () => searchStructures(search.value))
-;
+search.addEventListener('input', () => searchStructures(search.value));
